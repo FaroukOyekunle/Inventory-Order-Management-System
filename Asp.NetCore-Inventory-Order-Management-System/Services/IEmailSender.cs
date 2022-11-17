165875d5dfc36d732efa449fd97a8d28c95cd280
oyekunle farouk <oyekunlefarouk2001@gmail.com>
2022-11-17 17:46:31 +0100

    implement creation of the iemailsender interface with the sendemailasync method.

---
+++ Asp.NetCore-Inventory-Order-Management-System/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore_Inventory_Order_Management_System.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
