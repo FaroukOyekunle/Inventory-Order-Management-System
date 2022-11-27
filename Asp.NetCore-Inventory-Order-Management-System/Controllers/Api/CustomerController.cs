4f206c53c7faee416542145dce1f86faba8d927b
oyekunle farouk <oyekunlefarouk2001@gmail.com>
2022-11-27 16:59:17 +0100

    implement customer controller endpoint which is been authorize and has a action which will return the supported response content type (application/json format). And has a specified controller route attribute.

---
+++ Asp.NetCore-Inventory-Order-Management-System/Controllers/Api/CustomerController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Asp.NetCore_Inventory_Order_Management_System.Data;
+using Asp.NetCore_Inventory_Order_Management_System.Models;
+using Asp.NetCore_Inventory_Order_Management_System.Models.SyncfusionViewModels;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Asp.NetCore_Inventory_Order_Management_System.Controllers.Api
+{
+    [Authorize]
+    [Produces("application/json")]
+    [Route("api/Customer")]
+    public class CustomerController : Controller
+    {
+        private readonly ApplicationDbContext _applicationContext;
+
+        public CustomerController(ApplicationDbContext context)
+        {
+            _applicationContext = context;
+        }
+
+        // GET: api/Customer
+        [HttpGet]
+        public async Task<IActionResult> GetCustomer()
+        {
+            List<Customer> Items = await _applicationContext.Customer.ToListAsync();
+            int Count = Items.Count();
+            return Ok(new { Items, Count });
+        }
+
+
+        [HttpPost("[action]")]
+        public IActionResult Insert([FromBody]CrudViewModel<Customer> payload)
+        {
+            Customer customer = payload.value;
+            _applicationContext.Customer.Add(customer);
+            _applicationContext.SaveChanges();
+            return Ok(customer);
+        }
+
+        [HttpPost("[action]")]
+        public IActionResult Update([FromBody]CrudViewModel<Customer> payload)
+        {
+            Customer customer = payload.value;
+            _applicationContext.Customer.Update(customer);
+            _applicationContext.SaveChanges();
+            return Ok(customer);
+        }
+
+        [HttpPost("[action]")]
+        public IActionResult Remove([FromBody]CrudViewModel<Customer> payload)
+        {
+            Customer customer = _applicationContext.Customer
+                .Where(x => x.CustomerId == (int)payload.key)
+                .FirstOrDefault();
+            _applicationContext.Customer.Remove(customer);
+            _applicationContext.SaveChanges();
+            return Ok(customer);
+        }
+    }
+}
