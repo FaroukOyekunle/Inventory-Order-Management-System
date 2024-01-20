0b558e4a122e8f111cbbf1a4ce7cdae702b1be8d
Umar al Farouk <94355638+FaroukOyekunle@users.noreply.github.com>
2024-01-21 00:03:35 +0100

    implement customertype controller endpoint.

--- Asp.NetCore-Inventory-Order-Management-System/Controllers/CustomerTypeController.cs
+++ Asp.NetCore-Inventory-Order-Management-System/Controllers/CustomerTypeController.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Authorization;
 
 namespace Asp.NetCore_Inventory_Order_Management_System.Controllers
