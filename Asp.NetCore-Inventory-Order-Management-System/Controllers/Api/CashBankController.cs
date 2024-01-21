e67a2880abc3c0f519b770e32bbce241e5df3049
Umar al Farouk <94355638+FaroukOyekunle@users.noreply.github.com>
2024-01-22 00:23:21 +0100

    implement cash bank controller endpoint which is been authorize and has a action which will return the supported response content type (application/json format). And has a specified controller route attribute.

--- Asp.NetCore-Inventory-Order-Management-System/Controllers/Api/CashBankController.cs
+++ Asp.NetCore-Inventory-Order-Management-System/Controllers/Api/CashBankController.cs
@@ -1,14 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
 using Asp.NetCore_Inventory_Order_Management_System.Data;
 using Asp.NetCore_Inventory_Order_Management_System.Models;
 using Asp.NetCore_Inventory_Order_Management_System.Models.SyncfusionViewModels;
-using Microsoft.AspNetCore.Authorization;
 
 namespace Asp.NetCore_Inventory_Order_Management_System.Controllers.Api
 {
@@ -33,6 +33,7 @@ namespace Asp.NetCore_Inventory_Order_Management_System.Controllers.Api
             return Ok(new { Items, Count });
         }
 
+        // POST: api/CashBank
         [HttpPost("[action]")]
         public IActionResult Insert([FromBody]CrudViewModel<CashBank> payload)
         {
