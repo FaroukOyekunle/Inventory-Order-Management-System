6a1f3e43fb2cffc9248cbcf328548c5d4afecc9b
Umar al Farouk <94355638+FaroukOyekunle@users.noreply.github.com>
2024-01-24 12:31:18 +0100

    implement change password controller endpoint which is been authorize and has a action which will return the supported response content type (application/json format). And has a specified controller route attribute.

--- Asp.NetCore-Inventory-Order-Management-System/Controllers/Api/ChangePasswordController.cs
+++ Asp.NetCore-Inventory-Order-Management-System/Controllers/Api/ChangePasswordController.cs
@@ -1,13 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
 using Asp.NetCore_Inventory_Order_Management_System.Data;
 using Asp.NetCore_Inventory_Order_Management_System.Models;
 using Asp.NetCore_Inventory_Order_Management_System.Models.ManageViewModels;
 using Asp.NetCore_Inventory_Order_Management_System.Models.SyncfusionViewModels;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
+
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
 
