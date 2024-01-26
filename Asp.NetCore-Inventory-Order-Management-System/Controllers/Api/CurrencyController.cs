0163513bf827d84bf05b939890da1789d7097a1c
Umar al Farouk <94355638+FaroukOyekunle@users.noreply.github.com>
2024-01-26 14:06:36 +0100

    implement currency controller endpoint which is been authorize and has a action which will return the supported response content type (application/json format). And has a specified controller route attribute.

--- Asp.NetCore-Inventory-Order-Management-System/Controllers/Api/CurrencyController.cs
+++ Asp.NetCore-Inventory-Order-Management-System/Controllers/Api/CurrencyController.cs
@@ -5,10 +5,10 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
 using Asp.NetCore_Inventory_Order_Management_System.Data;
 using Asp.NetCore_Inventory_Order_Management_System.Models;
 using Asp.NetCore_Inventory_Order_Management_System.Models.SyncfusionViewModels;
-using Microsoft.AspNetCore.Authorization;
 
 namespace Asp.NetCore_Inventory_Order_Management_System.Controllers.Api
 {
@@ -33,6 +33,7 @@ namespace Asp.NetCore_Inventory_Order_Management_System.Controllers.Api
             return Ok(new { Items, Count });
         }
 
+        // GET: api/Branch
         [HttpGet("[action]/{id}")]
         public async Task<IActionResult> GetByBranchId([FromRoute]int id)
         {
