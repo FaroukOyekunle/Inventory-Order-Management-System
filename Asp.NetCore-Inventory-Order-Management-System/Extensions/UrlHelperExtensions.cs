d6ebab0b2b6e5e6cba53b84a5f3d1bead37f4420
Umar al Farouk <94355638+FaroukOyekunle@users.noreply.github.com>
2024-01-27 17:58:44 +0100

    implement url helper extension class which has a method EmailConfirmationLink and ResetPasswordCallbackLink.

--- Asp.NetCore-Inventory-Order-Management-System/Extensions/UrlHelperExtensions.cs
+++ Asp.NetCore-Inventory-Order-Management-System/Extensions/UrlHelperExtensions.cs
@@ -1,7 +1,7 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 using Asp.NetCore_Inventory_Order_Management_System.Controllers;
 
 namespace Microsoft.AspNetCore.Mvc
