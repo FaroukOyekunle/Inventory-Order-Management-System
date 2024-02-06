1c61a2bea5aaa190bc0d7a9b8ad2c22ed6656dec
Umar al Farouk <94355638+FaroukOyekunle@users.noreply.github.com>
2024-02-06 13:40:42 +0100

    Implement creation of domain models and view models

--- Asp.NetCore-Inventory-Order-Management-System/Models/ApplicationUser.cs
+++ Asp.NetCore-Inventory-Order-Management-System/Models/ApplicationUser.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Identity;
 
 namespace Asp.NetCore_Inventory_Order_Management_System.Models
