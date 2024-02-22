28c71804d7dd6863f2fe8b161ce9cbd592a43e37
Umar al Farouk <94355638+FaroukOyekunle@users.noreply.github.com>
2024-02-22 09:16:15 +0100

    Implement creation of domain models and view models

--- Asp.NetCore-Inventory-Order-Management-System/Models/InvoiceType.cs
+++ Asp.NetCore-Inventory-Order-Management-System/Models/InvoiceType.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Threading.Tasks;
 
 namespace Asp.NetCore_Inventory_Order_Management_System.Models
 {
