c965ee4400548c41fd509b9b1ac700b239d0ac4b
Umar al Farouk <94355638+FaroukOyekunle@users.noreply.github.com>
2024-03-02 06:58:54 -0800

    Implement creation of domain models and view models

--- Asp.NetCore-Inventory-Order-Management-System/Models/ManageViewModels/ExternalLoginsViewModel.cs
+++ Asp.NetCore-Inventory-Order-Management-System/Models/ManageViewModels/ExternalLoginsViewModel.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Linq;
-using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
 
