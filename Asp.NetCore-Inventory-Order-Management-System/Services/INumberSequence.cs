fac4f674fd0c46464d1455778c7444fac5c7b1e0
Umar al Farouk <94355638+FaroukOyekunle@users.noreply.github.com>
2024-03-09 13:10:04 -0800

    INumberSequence interface for managing and retrieving unique number sequences based on specified modules.

--- Asp.NetCore-Inventory-Order-Management-System/Services/INumberSequence.cs
+++ Asp.NetCore-Inventory-Order-Management-System/Services/INumberSequence.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Asp.NetCore_Inventory_Order_Management_System.Services
