4ae1b4b02521cf67f88e6d42633f1b66945528c4
Umar al Farouk <94355638+FaroukOyekunle@users.noreply.github.com>
2024-03-18 06:51:53 -0700

    Define SmtpOptions class to store SMTP configuration details required for sending emails, including username, password, host, port, SSL usage, sender information, and default status.

--- Asp.NetCore-Inventory-Order-Management-System/Services/SmtpOptions.cs
+++ Asp.NetCore-Inventory-Order-Management-System/Services/SmtpOptions.cs
@@ -7,13 +7,13 @@ namespace Asp.NetCore_Inventory_Order_Management_System.Services
 {
     public class SmtpOptions
     {
+        public string fromEmail { get; set; }
+        public string fromFullName { get; set; }
         public string smtpUserName { get; set; }
         public string smtpPassword { get; set; }
         public string smtpHost { get; set; }
         public int smtpPort { get; set; }
         public bool smtpSSL { get; set; }
-        public string fromEmail { get; set; }
-        public string fromFullName { get; set; }
         public bool IsDefault { get; set; }
 
     }
